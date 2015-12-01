6933abd3947617f50ee2ce3e7240f0acc20eea22
Mz <BalancedMz@gmail.com>
2015-12-01 23:24:15 +0800

    read Tables from file
    read Tables from file

--- src/simulator/CoeffStorage.java
+++ src/simulator/CoeffStorage.java
@@ -7,40 +7,41 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class CoeffStorage {
-	static Map<Integer , Integer> map = new HashMap<Integer , Integer>();
-	// Key-value hour=key
-	
-	
-	public static void readFile(File file) throws IllegalArgumentException
-	{
-		try {
-			//Todo : add exception checking for > 0 , < 24
-			Scanner scanner = new Scanner(file);
-			while(scanner.hasNext())
-			{
-			  int key = scanner.nextInt();
-			  int value = scanner.nextInt();
-			  if(key > 24 || value > 100 || key < 0 || value < 0)
-			    throw new IllegalArgumentException();
-				map.put(scanner.nextInt(), scanner.nextInt());
-				//scanner.nextLine();
-			}
-			
-			for (Map.Entry<Integer , Integer> entry : map.entrySet()) 
-			{ 
-				Integer key = entry.getKey(); 
-				Integer value = entry.getValue();
-				System.out.println(key + " : " + value);
-			}
-			scanner.close();
-			
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} 
-	}
-	
-	public static int getCoeff(int hour)
-	{
-		return map.get(hour);
-	}
+  static Map<Integer , Integer> map = new HashMap<Integer , Integer>();
+  // Key-value hour=key
+
+  /**
+   * Read the key-value pair from the file.
+   * @param file = The target file.
+   * @throws IllegalArgumentException = The stored data is wrong/ not fit the requirement.
+   */
+  @SuppressWarnings("resource")
+  public static void readFile(File file) throws IllegalArgumentException {
+    try {
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNext()) {
+        int key = scanner.nextInt();
+        int value = scanner.nextInt();
+        if (key > 24 || value > 100 || key < 0 || value < 0) {
+          throw new IllegalArgumentException();
+        }
+        System.out.println("Key read : " + key + " value : " + value);
+        map.put(key , value);
+        //scanner.nextLine();
+      }
+      System.out.println("size : " + map.size());
+      for (Map.Entry<Integer , Integer> entry : map.entrySet()) {
+        Integer key = entry.getKey(); 
+        Integer value = entry.getValue();
+        System.out.println(key + " : " + value);
+      }
+      scanner.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static int getCoeff(int hour) {
+    return map.get(hour);
+  }
 }
